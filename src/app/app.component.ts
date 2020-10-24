55c3d6286963fc0731df782e4f0d27fa640756a0
Hamilton Lima <hamilton.lima@gmail.com>
2020-10-23 23:51:37 -0400

    Adds blockly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxBlocklyConfig } from 'ngx-blockly/public_api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blockly-sandbox';
+  public config: NgxBlocklyConfig = {
+    toolbox: '<xml id="toolbox" style="display: none">' +
+                '<block type="controls_if"></block>' +
+                '<block type="controls_repeat_ext"></block>' +
+                '<block type="logic_compare"></block>' +
+                '<block type="math_number"></block>' +
+                '<block type="math_arithmetic"></block>' +
+                '<block type="text"></block>' +
+                '<block type="text_print"></block>' +
+             '</xml>',
+    scrollbars: true,
+    trashcan: true
+};
 }
