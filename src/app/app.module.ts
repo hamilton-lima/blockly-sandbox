032bbe0de2246b18bc3c7db3ffdf7885ed553125
Hamilton Lima <hamilton.lima@gmail.com>
2020-10-23 23:52:41 -0400

    Fixing the import, ha!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxBlocklyModule } from 'ngx-blockly/lib/ngx-blockly.module';
+import { NgxBlocklyModule } from 'ngx-blockly';
 
 @NgModule({
   declarations: [
